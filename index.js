4a9c913ea7df2438d1fcc2a5283e6ee0bbda9c16
devmahin <mahinhowlader123@gmail.com>
2024-01-30 22:46:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// let str = "I am mahin";
+// console.log(str.length)
+// console.log(str[3])
+// console.log(str[3])
+
+
+// let arr = [10, 20, 30, 40,50, 60, 70, 80, 90]
+// console.log(arr[3])
+// console.log(arr[1])
+// console.log(arr[5] = 1000)
+// console.log(arr)
+// console.log(arr.length)
+
+
+// toLowerCase()
+// let reg = "mahin";
+// let login = "Mahin";
+// if(reg.toLowerCase() === login.toLowerCase()){
+//     console.log("Login")
+// }else{
+//     console.log("wrong")
+// }
+
+
+
+// trim()
+let reg = "mahin ";
+let login = "Mahin   ";
+if(reg.trim().toLowerCase() === login.trim().toLowerCase()){
+    console.log("Login")
+}else{
+    console.log("wrong")
+}
+
+
+
+
+// string use slice
