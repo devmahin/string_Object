fe2c690e8eed4fc463f05869045194e9af8f45d2
devmahin <mahinhowlader123@gmail.com>
2024-01-31 07:17:36 +0600

    object

---
+++ object.js
@@ -0,0 +1,39 @@
+// const subject = {
+//     name : "Mahin",
+//     age : 30,
+//     roll : 1,
+//   "all subject" : ["bangla", "English","Math"],
+//   "all" : ["bangla", "English","Math"]
+
+// }
+
+// console.log(subject.name)
+// console.log(subject.age)
+// console.log(subject.roll)
+// console.log(subject["all subject"])
+// let all = "all"
+// console.log(subject[all] = ["aall docunent"])
+
+
+
+
+const collage = {
+        name : "Mahin",
+        class : [11 ,12],
+        unique : {
+            color : "orange",
+            result :{
+                gap: 5
+            }
+        }
+}
+// delete collage.class
+// console.log(collage.unique.result.gap);
+// console.log(collage["unique"]["result"]["gap"]);
+
+// console.log(collage)
+
+
+
+console.log(Object.keys(collage))
+console.log(Object.values(collage))
