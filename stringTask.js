905e1e4a3aa0803670f64b8ccfdab387a20fa735
devmahin <mahinhowlader123@gmail.com>
2024-02-01 15:32:53 +0600

    string task

--- stringTask.js
+++ stringTask.js
@@ -1,3 +1,25 @@
+// Count how many times a string has the letter a or A
+let str = "Praava Health is a network of Family Health Centers where patients come first. We are building a better Patient experience enabled by technology. We offer consultations with Family Medicine Doctors, Visiting Specialists, A Physiotherapists";
+// let match = str.match(/a/g);
+// let match2 = str.match(/A/g);
+// console.log(match.length)
+// console.log(match2.length)
+let Count = null;
+let Count2 = null;
+for(let i = 0; i<str.length; i++){
+    if(str[i] === "a"){
+        // console.log("")
+        Count++;
+    }
+    if(str[i] === "A"){
+        // console.log("")
+        Count2++;
+    }
+
+}
+console.log(Count)
+console.log(Count2)
+
 
 
 // // Count how many times a string has the letter a or A
@@ -18,17 +40,25 @@
 
 
 
-// Task-3:
-// Check whether a string contains all the vowels a, e, i, o, u
-
-let p = "Count how many times a string has the letter aA paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs."
-
-// for(let i = 0; i<= p.length; i++){
-//     console.log(p[i])
-//     if(p[i] === "a"){
-
+// // // Task-3:
+// // // Check whether a string contains all the vowels a, e, i, o, u
+// let a = "conut how many times a string";
+// let vowels = "aeiou";
+// let checked = true;
+// for (let i = 0; i < vowels.length; i++) {
+//     if (a.indexOf(vowels[i]) === -1) {
+//         checked = false;
+//         break;
 //     }
 // }
+
+// if (checked) {
+//     console.log("String contains all vowels");
+// } else {
+//     console.log("String is missing one or more vowels");
+// }
+
+
 
 
 
@@ -45,13 +75,21 @@ let p = "Count how many times a string has the letter aA paragraph is a series o
 
 
 
+
 //     Task-5:
 // Capitalize Every first Letter of each word in a String
-let par = "Google X Translate x bangladesh x"
-let words = par.split(" ")
 
-for(let i=0; i<words.length; i++){
-    // console.log()
-    let word = words[i]
-    let cap = word.charAt(0).toUpperCase() + word.slice("1")
-}
+
+// let par = "Google X Translate x bangladesh x"
+
+// let words = par.split(" ")
+// console.log(words)
+
+// let wordsCap = "";
+// for (let i = 0; i < words.length; i++) {
+//     // console.log()
+//     let word = words[i] + " ";
+//     // word[0] = 
+//     wordsCap += word.charAt(0).toUpperCase() + word.slice("1")
+// }
+// console.log(wordsCap)
